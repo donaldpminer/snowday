54ae5072de4e5bda1b9575cf1745593b4ac81467
Donald Miner <donaldpminer@gmail.com>
2014-01-25 09:57:46 -0500

    getting started with templates (forgot to commit snowday.py)

--- snowday.py
+++ snowday.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, redirect, url_for, escape, request
+from flask import Flask, session, redirect, url_for, escape, request, render_template, Markup
 from redis import StrictRedis
 from datetime import datetime
 
@@ -27,6 +27,10 @@ def today():
 def right_now():
     return str(datetime.now())
 
+
+@app.route('/test')
+def test():
+    return render_template('index.html', name='foo')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
